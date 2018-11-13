ab1a278603da5d3cbd9fe7e37c2f889172208ff7
“hananalshammary” <hanandarwish1993@gmail.com>
2018-11-13 13:15:12 +0300

    finished homework

---
+++ main.js
@@ -0,0 +1,58 @@
+/*var snakewaterMontana = {
+    paleontologist: "Dr. Alan Grant",
+    depth: "10 meters",
+    specimen: "Velociraptor"
+  };
+
+  var guestOfHonor = snakewaterMontana.paleontologist;
+  var cleverGirl = snakewaterMontana.specimen;
+
+ console.log(guestOfHonor);
+ console.log(cleverGirl);
+
+
+*/
+ var nicaragua = {
+  depth: "200 meters",
+  annualBudget: 1500000,
+  specimens: [
+    "Tyrannosaurus Rex",
+    "Stegosaurous",
+    "Triceratops",
+    "Velociraptor"
+  ]
+}
+
+var nicaraguanSpecimens =  nicaragua.specimens;
+console.log(nicaraguanSpecimens);
+
+var favoriteSpecimens =  nicaragua.specimens[1];
+
+nicaragu.annualBudget=  nicaragu.annualBudget + 250000;
+
+
+var hammondsMines = {
+  buenosAires: {
+    depth: "400 meters",
+    annualBudget: 1000000,
+    specimens: [
+      "Dilophosaurus",
+      "Brachiosaurus"
+    ]
+  },
+  mexico: {
+    depth: "350 meters",
+    annualBudget: 900000,
+    specimens: [
+      "Gallimimus",
+      "Parasaurolophus"
+    ]
+  }
+}
+
+console.log(hammondsMines.mexico['depth']);
+
+var mexicoMineDepth = hammondsMines.mexico['depth'];
+
+console.log(hammondsMines.buenosAires['annualBudget']);
+var buenosAiresannualBudget = hammondsMines.buenosAires['annualBudget'];
