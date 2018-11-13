41b1eddabfd47db576599a29a73702943fb9c17a
marwah14 <memo_224@live.com>
2018-11-13 12:32:24 +0300

    finished homework

---
+++ main.js
@@ -0,0 +1,61 @@
+snakewaterMontana = {
+    paleontologist: "Dr. Alan Grant",
+    depth: "10 meters",
+    specimen: "Velociraptor"
+  }
+  var guestOfHonor = ("Dr. Alan Grant");
+  var cleverGirl = ("Velociraptor");
+  console.log(snakewaterMontana.specimen);
+
+  nicaragua = {
+    depth: "200 meters",
+    annualBudget: 1500000,
+    specimens: [
+      "Tyrannosaurus Rex",
+      "Stegosaurous",
+      "Triceratops",
+      "Velociraptor"
+    ]
+  }
+
+  var nicaraguanSpecimens = [
+    "Tyrannosaurus Rex",
+      "Stegosaurous",
+      "Triceratops",
+      "Velociraptor"
+  ];
+  var favoriteSpecimen = ['TyrannosaurusRex', 'Stegosaurous', 'Triceratops', 'Velociraptor'];
+  var newAnnualBudget = 250000
+
+  hammondsMines = {
+    buenosAires: {
+      depth: "400 meters",
+      annualBudget: 1000000,
+      specimens: [
+        "Dilophosaurus",
+        "Brachiosaurus"
+      ]
+    },
+    mexico: {
+      depth: "350 meters",
+      annualBudget: 900000,
+      specimens: [
+        "Gallimimus",
+        "Parasaurolophus"
+      ]
+    }
+  }
+
+  console.log(hammondsMines.depth);
+  var depth = '350 meters';
+  var annualBudget = {
+
+  }
+  console.log(annualBudget[buenosAires]);
+  console.log(buenosAires[specimen]);
+
+  var annualBudget = [250000, 1000000, 900000];
+  var totalAnnualBudget = {
+    
+  }
+  var mexico = specimens[1];
