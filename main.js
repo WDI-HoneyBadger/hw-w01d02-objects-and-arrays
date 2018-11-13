b332f727a8de7db072373f580a727192f88a4050
masarsah <mascara.a.m.f@gmail.com>
2018-11-13 12:54:28 +0300

    add w01d02 homework

---
+++ main.js
@@ -0,0 +1,67 @@
+snakewaterMontana = {
+    paleontologist: "Dr. Alan Grant",
+    depth: "10 meters",
+    specimen: "Velociraptor"
+  }
+
+  var guestOfHonor = {paleontologist: "Dr. Alan Grant",}
+  console.log("specimen", snakewaterMontana.specimen);
+
+  snakewaterMontana.specimen;
+  var cleverGirl = "snakewaterMontana.specimen";
+
+console.log(cleverGirl);
+
+  nicaragua = {
+    depth: "200 meters",
+    annualBudget: 1500000,
+    specimens: [
+      "Tyrannosaurus Rex",
+      "Stegosaurous",
+      "Triceratops",
+      "Velociraptor"
+    ]
+  }
+
+  var nicaraguanSpecimens = nicaragua.specimen [
+    "Tyrannosaurus Rex",
+    "Stegosaurous",
+    "Triceratops",
+    "Velociraptor"
+  ]
+  var favoriteSpecimen = specimens.Triceratops;
+  console.log(favoriteSpecimen);
+
+  nicaragua.annualBudget = 250000 ;
+  
+
+
+hammondsMines = {
+  buenosAires: {
+    depth: "400 meters",
+    annualBudget: 1000000,
+    specimens: [l
+      "Dilophosaurus",
+      "Brachiosaurus"
+    ]
+  },
+  mexico: {
+    depth: "350 meters",
+    annualBudget: 900000,
+    specimens: [
+      "Gallimimus",
+      "Parasaurolophus"
+    ]
+  }
+}
+
+console.log(hammondsMines.mexico[0]);
+var red = hammondsMines.mexico[0];
+
+console.log(hammondsMines.buenosAires[1]);
+var blue = hammondsMines.buenosAires[1];
+
+console.log(hammondsMines.buenosAires[1]);
+var annualBudget= 60000;
+
+console.log(Parasaurolophus.specimens[1]);
