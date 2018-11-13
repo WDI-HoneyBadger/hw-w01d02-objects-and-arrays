79a35d8358c8d2aa926cdc7c3a538248ed4b3824
Hamad Alshehri <eng_cis1@hotmail.comL>
2018-11-13 11:51:44 +0300

    add homework

---
+++ homworkW1.js
@@ -0,0 +1,47 @@
+/*snakewaterMontana = {
+    paleontologist: "Dr. Alan Grant",
+    depth: "10 meters",
+    specimen: "Velociraptor"
+  }
+  var guestOfHonor=snakewaterMontana.paleontologist;
+  console.log(guestOfHonor);
+  var cleverGirl=snakewaterMontana.specimen;
+  console.log(cleverGirl);*/
+
+  nicaragua = {
+    depth: "200 meters",
+    annualBudget: 1500000,
+    specimens: [
+      "Tyrannosaurus Rex",
+      "Stegosaurous",
+      "Triceratops",
+      "Velociraptor"
+    ]
+  }
+  var nicaraguanSpecimens=nicaragua.specimens;
+  /*console.log(nicaraguanSpecimens);*/
+
+  var favoriteSpecimen = nicaraguanSpecimens[1,2];
+console.log(favoriteSpecimen);
+ var adding= nicaragua.annualBudget+250000;
+ console.log(adding);
+
+
+ hammondsMines = {
+    buenosAires: {
+      depth: "400 meters",
+      annualBudget: 1000000,
+      specimens: [
+        "Dilophosaurus",
+        "Brachiosaurus"
+      ]
+    },
+    mexico: {
+      depth: "350 meters",
+      annualBudget: 900000,
+      specimens: [
+        "Gallimimus",
+        "Parasaurolophus"
+      ]
+    }
+  }
