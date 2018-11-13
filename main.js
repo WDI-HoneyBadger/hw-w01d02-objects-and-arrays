382b2cbb6761e5a56329d6892f2a14da5083ee36
mohrahalateeq <m.alateeq.93@hotmail.com>
2018-11-13 14:26:14 +0300

    add w01d02 homework

---
+++ main.js
@@ -0,0 +1,52 @@
+
+snakewaterMontana = {
+    guestOfHonor : "Dr. Alan Grant" ,
+    paleontologist: "Dr. Alan Grant",
+    depth: "10 meters",
+    specimen: "Velociraptor"
+  }
+    var cleverGirl = snakewaterMontana[specimen];
+    console.log(cleverGirl);
+
+    nicaragua = {
+      depth: "200 meters",
+      annualBudget: 1500000 + 250000,
+      specimens: [
+        "Tyrannosaurus Rex",
+        "Stegosaurous",
+        "Triceratops",
+        "Velociraptor"
+      ]
+  
+    }
+  nicaraguanSpecimens = specimens ;
+  favoriteSpecimen = nicaraguanSpecimens[2];
+
+  javascript
+  hammondsMines = {
+    buenosAires: {
+      depth: "400 meters",
+      annualBudget: 1000000,
+      specimens: [
+        "Dilophosaurus",
+        "Brachiosaurus"
+      ]
+  
+    },
+    mexico: {
+      depth: "350 meters",
+      annualBudget: 900000,
+      specimens: [
+        "Gallimimus",
+        "Parasaurolophus"
+      ]
+    }
+    
+}
+  console.log(hammondsMines.depth)
+        var appropriately= mexico.depth;
+    var para ='Parasaurolophus';
+    console.log(mexico[para]);
+    console.log(mexico.specimens[1]);
+    hammondsMines = nicaragua;
+    console.log(nicaragua);
