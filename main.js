ed98c4fa934f6ac4f9bf68da149de609206a85ff
Bader1011 <ibader1011@gmail.com>
2018-11-13 17:15:50 +0300

    homework is finished

---
+++ main.js
@@ -0,0 +1,64 @@
+
+snakewaterMontana = {
+    uestOfHonor = ["Snakewater", "Montana"];
+      paleontologist: "Dr. Alan Grant",
+      depth: "10 meters",
+      specimen: "Velociraptor"
+    }
+  var guestOfHonor = 'paleontologist';
+  var cleverGirl = (snakewaterMontana ['specimen']);
+  var NameOfPatSM=(snakewaterMontana['paleontologist']);
+  console.log(cleverGirl);
+  console.log(NameOfPatSM);
+  
+    nicaragua = {
+      depth: "200 meters",
+      annualBudget: 1500000,
+      specimens: [
+        "Tyrannosaurus Rex",
+        "Stegosaurous",
+        "Triceratops",
+        "Velociraptor"
+      ]
+      //alert ("annualBudget" + 250000);
+  
+     /* annualBudget.toString();
+      (1500000).toString();
+      (1500000)+ (250000).toString ();*/
+  
+  
+     
+  
+  
+    }
+  var nicaraguanSpecimens. = nicaragua;  
+  var favoriteSpecimen ="";
+  
+  
+    hammondsMines = {
+      buenosAires: {
+        depth: "400 meters",
+        annualBudget: 1000000,
+        specimens: [
+          "Dilophosaurus",
+          "Brachiosaurus"
+        ]
+        
+      },
+      mexico: {
+        depth: "350 meters",
+        annualBudget: 900000,
+        specimens: [
+          "Gallimimus",
+          "Parasaurolophus"
+        ]
+      }
+      hammondsMines = hammondsMines[depth.length -1 ]
+    }
+  
+  
+  
+  var uestOfHonor = ["Snakewater", "Montana"];
+  uestOfHonor=  "paleontologist";
+  
+  
