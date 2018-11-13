3d5c7715e6d202fec8f7aad6758487a9343c1859
trevorpreston <trevorpreston4@gmail.com>
2018-11-13 11:42:00 +0300

    add w01d02 homework

---
+++ main.js
@@ -0,0 +1,8 @@
+snakewaterMontana = {
+  paleontologist: "Dr. Alan Grant",
+  depth: "10 meters",
+  specimen: "Velociraptor"
+}
+
+var guestOfHonor = snakewaterMontana.paleontologist;
+console.log(guestOfHonor);
