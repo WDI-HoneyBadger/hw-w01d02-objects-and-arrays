6d2cbd9e08ea74f8e505c191ed90801e89e1b0b4
rawandahli <rawandahlii@gmail.com>
2018-11-13 21:07:48 +0300

    finished homework

---
+++ main.js
@@ -0,0 +1,70 @@
+
+     
+
+var snakewaterMontana = {
+    paleontologist: "Dr. Alan Grant",
+    depth: "10 meters",
+    specimen: "Velociraptor"
+  }
+  var guestOfHonor = snakewaterMontana.paleontologist;
+
+     var cleverGirl = snakewaterMontana.specimen;
+
+     console.log (guestOfHonor);
+ console.log (cleverGirl);
+
+ nicaragua = {
+    depth: "200 meters",
+    annualBudget: 1500000,
+    specimens: [
+      "Tyrannosaurus Rex",
+      "Stegosaurous",
+      "Triceratops",
+      "Velociraptor"
+    ]
+  }
+ 
+  annualBudget = annualbudget + 250000;
+  console.log (annualBudget);
+
+  var favoriteSpecimen = specimens[2]
+  var nicaraguanSpecimens = nicaragua.specimens
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+     nicaragua = {
+        depth: "200 meters",
+        annualBudget: 1500000,
+        specimens: [
+          "Tyrannosaurus Rex",
+          "Stegosaurous",
+          "Triceratops",
+          "Velociraptor"
+        ]
+    }
+     var nicaraguanSpecimens = nicaragua.specimens;
+     var favoriteSpecimen = nicaragua.specimens[0];
+     console.log (nicaraguanSpecimens);
+
+
+
