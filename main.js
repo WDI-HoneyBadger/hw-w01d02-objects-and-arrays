1abc8067bbb7725884ec2068d898189a7b77b173
Masarah Fallath <masarah@MacBook-Air-alkhas-b-Masarah.local>
2018-11-18 19:13:25 +0300

    re write

--- main.js
+++ main.js
@@ -1,46 +1,40 @@
+
 snakewaterMontana = {
     paleontologist: "Dr. Alan Grant",
     depth: "10 meters",
     specimen: "Velociraptor"
   }
+//........PART 1.........//
 
   var guestOfHonor = {paleontologist: "Dr. Alan Grant",}
-  console.log("specimen", snakewaterMontana.specimen);
+  var cleverGirl = snakewaterMontana.specimen;
+  console.log(cleverGirl);
+//.......................//
 
-  snakewaterMontana.specimen;
-  var cleverGirl = "snakewaterMontana.specimen";
-
-console.log(cleverGirl);
-
-  nicaragua = {
-    depth: "200 meters",
-    annualBudget: 1500000,
-    specimens: [
-      "Tyrannosaurus Rex",
-      "Stegosaurous",
-      "Triceratops",
-      "Velociraptor"
-    ]
-  }
-
-  var nicaraguanSpecimens = nicaragua.specimen [
+nicaragua = {
+  depth: "200 meters",
+  annualBudget: 1500000,
+  specimens: [
     "Tyrannosaurus Rex",
     "Stegosaurous",
     "Triceratops",
     "Velociraptor"
   ]
-  var favoriteSpecimen = specimens.Triceratops;
-  console.log(favoriteSpecimen);
+}
+//........PART 2.........//
+var nicaraguanSpecimens = nicaragua["specimen"]
+var favoriteSpecimen = nicaragua.specimens[2];
+console.log(favoriteSpecimen);
+nicaragua.annualBudget =(250000)
 
-  nicaragua.annualBudget = 250000 ;
-  
-
+console.log(nicaragua["annualBudget"]);
+//.........................//
 
 hammondsMines = {
   buenosAires: {
     depth: "400 meters",
     annualBudget: 1000000,
-    specimens: [l
+    specimens: [
       "Dilophosaurus",
       "Brachiosaurus"
     ]
@@ -55,13 +49,48 @@ hammondsMines = {
   }
 }
 
-console.log(hammondsMines.mexico[0]);
-var red = hammondsMines.mexico[0];
 
-console.log(hammondsMines.buenosAires[1]);
-var blue = hammondsMines.buenosAires[1];
+//........PART 3.........//
+//1
+hammondsMines.mexico.depth;
+//2
+var hammondsMiness = hammondsMines.mexico.depth
+ console.log(hammondsMiness)
+//3
+ hammondsMines.buenosAires.annualBudget
+ console.log( hammondsMines.buenosAires.annualBudget)
 
-console.log(hammondsMines.buenosAires[1]);
-var annualBudget= 60000;
+//4
+ var annualBudgett = hammondsMines.buenosAires.annualBudget
+ console.log(annualBudgett)
+ hammondsMines.buenosAires.annualBudget
+//5
+hammondsMines.buenosAires.specimens[0]
+//6
+hammondsMines["nicaragua"] = {
+  depth: "200 meters",
+  annualBudget: 1500000,
+  specimens: [
+    "Tyrannosaurus Rex",
+    "Stegosaurous",
+    "Triceratops",
+    "Velociraptor"
+  ]
+}
 
-console.log(Parasaurolophus.specimens[1]);
+
+console.log(hammondsMines)
+ //7
+ var storeAnual1 = hammondsMines.buenosAires.annualBudget;
+ var storeAnual2 = hammondsMines.mexico.annualBudget;
+ var storeAnual3 = hammondsMines.nicaragua.annualBudget;
+ 
+ //8
+ var totalAnnualCost = storeAnual1+ storeAnual2 + storeAnual3
+ console.log(totalAnnualCost)
+
+
+//9
+hammondsMines.mexico.specimens[1];
+console.log(hammondsMines.mexico.specimens[1])
+ 
