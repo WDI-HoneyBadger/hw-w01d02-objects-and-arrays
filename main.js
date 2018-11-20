e68f554a34c23d9ac55668794ca71d6c01daf3f8
Turki Alomari <t-matar@windowslive.com>
2018-11-20 17:36:13 +0300

    done homework

---
+++ main.js
@@ -0,0 +1,48 @@
+
+snakewaterMontana = {
+    paleontologist: "Dr. Alan Grant",
+    depth: "10 meters",
+    specimen: "Velocirapto"
+};
+
+var guestOfHonor = snakewaterMontana.paleontologist;
+var cleverGirl = snakewaterMontana.specimen;
+console.log (cleverGirl);
+
+nicaragua = {
+    depth: "200 meters",
+    annualBudget: 1500000,
+    specimens: [
+      "Tyrannosaurus Rex",
+      "Stegosaurous",
+      "Triceratops",
+      "Velociraptor"
+    ]
+  }
+ var specimens = nicaraguanSpecimens 
+ var favoriteSpecimen = nicaraguanSpecimens.velociraptor;
+ console.log (velociraptor)
+ nicaragua=(annualBudget= 400000);
+ console.log (annualBudget ="400000")
+
+
+  hammondsMines = {
+    buenosAires: {
+      depth: "400 meters",
+      annualBudget: 1000000,
+      specimens: [
+        "Dilophosaurus",
+        "Brachiosaurus"
+      ]
+    },
+    mexico: {
+      depth: "350 meters",
+      annualBudget: 900000,
+      specimens: [
+        "Gallimimus",
+        "Parasaurolophus"
+      ]
+    }
+  }
+
+
