a3514196f9cfcc17973139ed67ba37c09c8df8a1
marwah14 <memo_224@live.com>
2018-11-23 11:06:51 +0300

    done

--- main.js
+++ main.js
@@ -3,9 +3,17 @@ snakewaterMontana = {
     depth: "10 meters",
     specimen: "Velociraptor"
   }
-  var guestOfHonor = ("Dr. Alan Grant");
-  var cleverGirl = ("Velociraptor");
-  console.log(snakewaterMontana.specimen);
+  //part 1
+  var guestOfHonor = snakewaterMontana.paleontologist;
+    console.log(guestOfHonor);
+
+  
+  //2
+
+var cleverGirl = snakewaterMontana.specimen;
+console.log(cleverGirl);
+
+  //part 2
 
   nicaragua = {
     depth: "200 meters",
@@ -17,15 +25,19 @@ snakewaterMontana = {
       "Velociraptor"
     ]
   }
+//1 
+var nicaraguanSpecimens = nicaragua.specimens;
+console.log(nicaraguanSpecimens);
+//2
+var favoriteSpecimen = nicaraguanSpecimens;
+console.log(nicaraguanSpecimens[1]);
 
-  var nicaraguanSpecimens = [
-    "Tyrannosaurus Rex",
-      "Stegosaurous",
-      "Triceratops",
-      "Velociraptor"
-  ];
-  var favoriteSpecimen = ['TyrannosaurusRex', 'Stegosaurous', 'Triceratops', 'Velociraptor'];
-  var newAnnualBudget = 250000
+//3
+
+console.log(nicaragua.annualBudget + 250000);
+
+
+//part 3
 
   hammondsMines = {
     buenosAires: {
@@ -45,17 +57,49 @@ snakewaterMontana = {
       ]
     }
   }
+//1
+  console.log(hammondsMines.mexico.depth);
+  //2
 
-  console.log(hammondsMines.depth);
-  var depth = '350 meters';
-  var annualBudget = {
+  //3
+  console.log(hammondsMines.buenosAires.annualBudget);
 
-  }
-  console.log(annualBudget[buenosAires]);
-  console.log(buenosAires[specimen]);
+//4
+var newBudget = hammondsMines.buenosAires.annualBudget;
+console.log(newBudget);
+//5
+hammondsMines.buenosAires.specimens.push('DNA');
+console.log(hammondsMines.buenosAires.specimens);
 
-  var annualBudget = [250000, 1000000, 900000];
-  var totalAnnualBudget = {
+
+//6 
+hammondsMines.nicaragua = nicaragua;
+console.log(hammondsMines);
+
+
+//7 
+var newB = hammondsMines.buenosAires.annualBudget;
+console.log(newB);
+var newB2 = hammondsMines.mexico.annualBudget;
+console.log(newB2);
+var newB3 = hammondsMines.nicaragua.annualBudget;
+console.log(newB3);
+
+// 8 
+var totalAnnualBudget = [newB, newB2, newB3];
+console.log(totalAnnualBudget);
+
+//9 
+console.log(hammondsMines.mexico.specimens[1]);
+
+
+
+
+  //console.log(annualBudget[buenosAires]);
+  //console.log(buenosAires[specimen]);
+
+  //var annualBudget = [250000, 1000000, 900000];
+  //var totalAnnualBudget = {
     
-  }
-  var mexico = specimens[1];
+  //}
+  
